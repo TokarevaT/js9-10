30d99bd316c1b652f7202fd5b1fb18cb4ed7feda
Tokareva Tanya <tokareva-tatyanka@mail.ru>
2016-05-10 15:57:19 +0300

    d

--- js9-10/js/JavaScript1.js
+++ js9-10/js/JavaScript1.js
@@ -43,6 +43,35 @@ $(function () {
   }, 'fast'
 );
 
+    var site = function() {
+	this.navLi = $('#nav li').children('ul').hide().end();
+	this.init();
+};
+site.prototype = {
+ 	init : function() {
+ 		this.setMenu();
+ 	},
+ 	// Enables the slidedown menu, and adds support for IE6
+ 	setMenu : function() {
+ 	$.each(this.navLi, function() {
+ 		if ( $(this).children('ul')[0] ) {
+ 			$(this)
+ 				.append('<span />')
+ 				.children('span')
+ 					.addClass('hasChildren')
+ 		}
+ 	});
+ 		this.navLi.hover(function() {
+ 			// mouseover
+			$(this).find('> ul').stop(true, true).slideDown('slow', 'easeOutBounce');
+ 		}, function() {
+ 			// mouseout
+ 			$(this).find('> ul').stop(true, true).hide(); 		
+		});
+ 	}
+}
+new site();
+
 
     jQuery(".niceCheck").each(
     /* при загрузке страницы меняем обычные на стильные checkbox */
